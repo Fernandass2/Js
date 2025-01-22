6baec9fcf6be6925d9891b21f7e4e547f5a998b3
FERNANDA BATISTA NERIS <fernandinhabatista78@hotmail.com>
2025-01-22 16:37:51 -0300

    Funçoes

--- fundamentos/funcoes.js
+++ fundamentos/funcoes.js
@@ -6,4 +6,47 @@
 function hello(){
     console.log("Hello function")
 }
+console.log(typeof(hello))
 hello()
+
+// Função atribuida
+const hello2 = function (){
+    console.log("Hello function assigned") 
+}
+
+console.log(typeof(hello2))
+hello2()
+
+
+// Arrow Function -> (Simplificação da função atribuida)
+// Function -----  =>
+const hello3 = () => {
+    console.log("Hello arrow fuction")
+}
+console.log(typeof(hello3))
+hello3()
+
+
+// Funções com passagem de parâmetros e retorno
+// Função simples
+function somarS(num1, num2) {
+    return (console.log(num1 - num2))
+}
+somarS(10, 3)
+
+// Função Atribuida
+const somarA = function(num1, num2){
+    return (console.log(num1 + num2))
+}
+somarA (4, 5)
+
+// Arrow function
+const somarAF = (num1, num2) => {
+    return (console.log(num1 + num2))
+}
+somarAF(5, 7)
+
+// Arrow function simplificada (retorno é implicito)
+const somarAFS = (num1, num2) => console.log(num1 + num2)
+
+somarAFS (2, 2)
