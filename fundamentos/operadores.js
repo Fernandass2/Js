978ec96e729b0105f8f5588eefba76a706b44016
FERNANDA BATISTA NERIS <fernandinhabatista78@hotmail.com>
2025-01-22 17:14:16 -0300

    Operadores

---
+++ fundamentos/operadores.js
@@ -0,0 +1,34 @@
+/**
+ *  OPERADORES
+ * - Aritmétricos de atribuição
+ * - Comparação 
+ * - Lógicos
+ */
+
+console.clear()
+console.log("Operadores aritmétricos")
+console.log("+ | Soma")
+console.log("- | Subtração")
+console.log("* | Multiplicação")
+console.log("/ | Divisão")
+console.log("% | Módulo (resto da divisão")
+console.log("")
+console.log("Operadores aritmétricos de atribuição")
+console.log("+ | Soma e atribuição a variável")
+console.log("- | Subtração e atribuição a variável")
+console.log("* | Multiplicação e atribuição a variável")
+console.log("/ | Divisão e atribuição a variável")
+console.log("+= | Soma e atribuição a variável")
+console.log("-= | Subtração e atribuição a variável")
+console.log("*= | Multiplicação e atribuição a variável")
+console.log("/= | Divisão e atribuição a variável")
+console.log("++ | Somar 1 ao resultado")
+console.log("-- | Subtrair 1 ao resultado")
+console.log("")
+console.log("Compradores")
+console.log("> | Maior que")
+console.log("< | Menor que")
+console.log(">= | Maior ou igual a")
+console.log(">= | Maior ou igual a")
+console.log("=== | Indênticos a")
+console.log("!== | Não indênticos a")
